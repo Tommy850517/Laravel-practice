c00aade0277e3e951a9b8e46582148d0639d8610
Ubuntu <ubuntu@ip-172-31-29-210.us-west-2.compute.internal>
2023-02-07 02:23:35 +0000

    init commit

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class FirstController extends Controller
+{
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+	
+
+	public function cv(){
+	    return view('mycv');
+	}
+
+}
